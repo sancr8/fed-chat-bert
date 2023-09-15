7e5a86f889edb79033dfc9b6363fcce1db6a8d1e
~dskh@nal~ <dskhanal@gmail.com>
2023-09-15 22:12:54 +1000

    Update checkpoint.py

--- checkpoint.py
+++ checkpoint.py
@@ -6,8 +6,17 @@ model_name = "bert-base-uncased"  # You can choose a specific variant
 model = BertForMaskedLM.from_pretrained(model_name)
 tokenizer = BertTokenizer.from_pretrained(model_name)
 
+# Freeze the weights of the pre-trained model
+for param in model.parameters():
+    param.requires_grad = False
+    
 # Save the model's state_dict to a checkpoint file
 checkpoint_path = "bert_checkpoint.pth"
 torch.save(model.state_dict(), checkpoint_path)
+
+# Initialize the model with random weights
+num_classes = 1000
+model.classifier = torch.nn.Linear(768, num_classes)
+model.init_weights()
 
 print(f"Pre-trained BERT model '{model_name}' checkpoint saved to {checkpoint_path}")
